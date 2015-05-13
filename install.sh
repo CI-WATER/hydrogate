6b626d5a9ee18e525bf068b0035b93fb51219017
ahmetartu <ahmetartu@aggiemail.usu.edu>
2015-05-13 01:16:39 -0600

    username fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo /etc/init.d/lighttpd stop
-cp ./dist/Release/GNU-Linux-x86/hydrogate /home/hydrogate/www/bin/
-cp configfastcgi.js /home/hydrogate/www/bin/
+cp ./dist/Release/GNU-Linux-x86/hydrogate /home/demouser/www/bin/
+cp configfastcgi.js /home/demouser/www/bin/
 sudo /etc/init.d/lighttpd start
